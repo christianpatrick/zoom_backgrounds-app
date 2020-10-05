fcc3061aad0bfd9027fd3242428212953846d314
Christian Helvin <hi@mynameischristian.com>
2020-10-04 22:57:28 -0500

    Added video download

--- src/components/MediaList.svelte
+++ src/components/MediaList.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte"
+    import { saveAs } from "file-saver"
 
     export let searchValue
     export let mediaUrl = ""
@@ -48,6 +49,23 @@
         const thisMediaItem = root.querySelector(`[data-id*="${thisMediaId}"]`)
         thisMediaItem.classList.add("active")
     }
+
+    const downloadMedia = (mediaId) => {
+        let mediaDetails = mediaList.filter((el) => el.id == mediaId)
+        let supportedVideoFiles = mediaDetails[0].video_files.filter(
+            (file) => file.width >= 640 && file.width <= 1280
+        )
+
+        supportedVideoFiles.sort((a, b) => {
+            return a.width < b.width
+        })
+
+        fetch(supportedVideoFiles[0].link, {
+            responseType: "blob",
+        })
+            .then((response) => response.blob())
+            .then((blob) => saveAs(blob, `${mediaId}.mp4`))
+    }
 </script>
 
 <style>
@@ -57,13 +75,20 @@
         text-align: center;
     }
 
+    div {
+        position: relative;
+        width: 350px;
+        height: 195px;
+        float: left;
+        margin: 0.5rem;
+    }
+
     img {
         width: 350px;
         height: 195px;
         background-color: #ffffff;
         border-radius: 1rem;
         border: 0.5rem solid #ffffff;
-        margin: 0.5rem;
         object-fit: cover;
         cursor: pointer;
         transition: 0.3s;
@@ -77,15 +102,43 @@
         border-color: #6396ff !important;
         box-shadow: 0 0 3rem 0 rgba(0, 0, 0, 0.2);
     }
+
+    svg {
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
+        color: rgba(255, 255, 255, 0.7);
+        height: 2.25rem;
+        cursor: pointer;
+        padding: 10px;
+    }
+
+    svg:hover {
+        color: #ffffff;
+    }
 </style>
 
 <section bind:this={root}>
     {#each mediaList as media}
-        <img
-            on:click={selectMedia}
-            alt={`Video by ${media.user.name}`}
-            data-id={media.id}
-            data-video={media.video_files[0].link}
-            src={media.image} />
+        <div>
+            <img
+                on:click={selectMedia}
+                alt={`Video by ${media.user.name}`}
+                data-id={media.id}
+                data-video={media.video_files[0].link}
+                src={media.image} />
+            <svg
+                on:click={downloadMedia(media.id)}
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="download"
+                class="svg-inline--fa fa-download fa-w-16"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"><path
+                    fill="currentColor"
+                    d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z" /></svg>
+        </div>
     {/each}
 </section>
